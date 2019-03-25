e152ef14954f4a365e44eb2a9a8d4e02bac0d327
Василий Чапаев <chapaevpustota@gmail.com>
2019-03-25 19:44:18 +0300

    2

---
+++ db.js
@@ -0,0 +1,105 @@
+const { MongoClient } = require("mongodb");
+const url = require("./config.json").mongo_url;
+const subscribe_event = require("./subscribe_event");
+
+let db;
+let close;
+
+module.exports = () => {
+    return MongoClient.connect(url, { useNewUrlParser: true }).then(client => {
+        db = client.db("solaris");
+        return client;
+    });
+};
+
+module.exports.Stream = {
+    all() {
+        return db
+            .collection("streamers")
+            .find()
+            .toArray();
+    },
+
+    all_in_chanell(channel) {
+        return db
+            .collection("streamers")
+            .find({ channels: { $in: [channel] } })
+            .toArray();
+    },
+
+    add(streamer_id, channel, streamer_name) {
+        return db
+            .collection("streamers")
+            .find({ streamer_id: streamer_id })
+            .toArray()
+            .then(result => {
+                if (result[0]) {
+                    if (result[0].channels.indexOf(channel) == -1) {
+                        let new_channels = result[0].channels;
+                        new_channels.push(channel);
+                        return db
+                            .collection("streamers")
+                            .updateOne(
+                                { streamer_id: streamer_id },
+                                { $set: { channels: new_channels } }
+                            )
+                            .then(result => `Стример ${streamer_name} добавлен в список`);
+                    } else {
+                        return `Стример ${streamer_name} уже в списке`;
+                    }
+                } else {
+                    let streamer = {
+                        name: streamer_name,
+                        streamer_id: streamer_id,
+                        channels: [channel]
+                    };
+                    subscribe_event("subscribe", streamer_id);
+                    return db
+                        .collection("streamers")
+                        .insertOne(streamer)
+                        .then(result => `Стример ${streamer_name} добавлен в список`);
+                }
+            });
+    },
+
+    get(streamer_id) {
+        return db
+            .collection("streamers")
+            .find({ streamer_id: +streamer_id })
+            .toArray();
+    },
+
+    remove(streamer_id, channel) {
+        return db
+            .collection("streamers")
+            .find({ streamer_id: streamer_id })
+            .toArray()
+            .then(result => {
+                if (result[0]) {
+                    let i = result[0].channels.indexOf(channel);
+                    if (i != -1) {
+                        let new_channels = result[0].channels;
+                        new_channels.splice(i, 1);
+                        if (new_channels.length === 0) {
+                            return db
+                                .collection("streamers")
+                                .deleteOne({ streamer_id: streamer_id })
+                                .then(result => `Стример удалён из списка`);
+                        } else {
+                            return db
+                                .collection("streamers")
+                                .updateOne(
+                                    { streamer_id: streamer_id },
+                                    { $set: { channels: new_channels } }
+                                )
+                                .then(result => `Стример удалён из списка`);
+                        }
+                    } else {
+                        return "Такого стримера нет в списке";
+                    }
+                } else {
+                    return "Такого стримера нет в списке";
+                }
+            });
+    }
+};
